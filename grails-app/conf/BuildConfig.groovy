4cd61bdfdf9439debe81ac8cd07ac3e25c39efb0
Franjo Žilić <frenky666@gmail.com>
2012-07-20 07:18:14 +0200

    Changed to new plugin version

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -43,7 +43,7 @@ grails.project.dependency.resolution = {
         runtime ":hibernate:$grailsVersion"
         runtime ":jquery:1.7.1"
         runtime ":resources:1.1.6"
-        compile ":mybatis:0.0.1"
+        compile ":mybatis:0.0.2"
 
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0"
